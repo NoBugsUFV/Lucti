c3d2e8334b9e62729a2fbfbb8ca6d0993f3708d8
Thiago <thiagofp707@gmail.com>
2020-06-12 16:13:07 -0300

    Pacote de navegação adicionado

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import Routes from './src/routes';
-
+import Login from './src/pages/Login';
 
 export default function App() {
   return (
-    <Routes/>
+    <Login/>
   );
 }
 
