23b58eea80090c35fd560faa74a93d900dc753db
Júlio Alves <julio619alves@gmail.com>
2020-06-12 19:28:29 -0300

    creates menu layout

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import {Title, Caption, Searchbar} from 'react-native-paper';
-import { render } from 'react-dom';
+import {Card, Button, Avatar, Title, Paragraph, Caption, Searchbar} from 'react-native-paper';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const Circle = props => <View style={styles.circle}></View>
+const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+
+const categories = [
+    {id: 1, title: 'Vestuário', picture: 'https://picsum.photos/700'},
+    {id: 2, title: 'Bebidas', picture: 'https://picsum.photos/700'},
+    {id: 3, title: 'Moda', picture: 'https://picsum.photos/700'},
+    {id: 4, title: 'Beleza', picture: 'https://picsum.photos/700'},
+    {id: 5, title: 'Transportes', picture: 'https://picsum.photos/700'},
+    {id: 6, title: 'Esportes', picture: 'https://picsum.photos/700'},
+    {id: 7, title: 'Mecânicas', picture: 'https://picsum.photos/700'},
+    {id: 8, title: 'Eletrônicos', picture: 'https://picsum.photos/700'}
+]
+
+export const Categories = props => 
+categories.map(categorie => {
+    return (
+        <Card style={styles.card} key={categorie.id}>
+            <Card.Cover source={{ uri: categorie.picture }} />
+            <Card.Content>
+                <Title style={{lineHeight: 20}}>{categorie.title}</Title>
+            </Card.Content>
+        </Card>
+    )
+})
 
 export default class Home extends React.Component{
     state = {
@@ -23,12 +47,22 @@ export default class Home extends React.Component{
                     <Searchbar placeholder="Busque por empresas ou serviços" onChangeText={this._onChangeSearch} value={searchQuery}/>
                 </View>
                 <View style={styles.center}>
-                    <Circle/>
-                    <Circle/>
-                    <Circle/>
+                    <View style={{flexDirection: 'row'}}>
+                        <Title style={{flex: 5}}>Categorias</Title>
+                        <Caption style={{flex: 1}}>Ver todas</Caption>
+                    </View>
+                    <ScrollView horizontal={true}>
+                        <Categories></Categories>
+                    </ScrollView>
                 </View>
                 <View style={styles.south}>
-                    <Circle/>
+                    <View style={{flexDirection: 'row'}}>
+                        <Title style={{flex: 5}}>Populares</Title>
+                        <Caption style={{flex: 1}}>Ver todas</Caption>
+                    </View>
+                    <ScrollView style={{width: '90%'}}>
+                        <Categories></Categories>
+                    </ScrollView>
                 </View>
             </View>
         );
@@ -41,21 +75,20 @@ const styles = StyleSheet.create({
         height: '100%'
     },
     north: {
-        flex: 1,
+        flex: 1.5,
         backgroundColor: '#76AB82',
         alignItems: 'center',
         justifyContent: 'center'
     },
     center: {
-        flex: 1,
-        flexDirection: 'row',
+        flex: 2,
+        flexDirection: 'column',
         backgroundColor: '#F9F9F9',
-        alignItems: 'center',
         justifyContent: 'space-between',
-        paddingHorizontal: 10
+        paddingHorizontal: 10,
     },
     south: {
-        flex: 1,
+        flex: 2,
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#F9F9F9'
@@ -69,10 +102,15 @@ const styles = StyleSheet.create({
     title: {
 		fontSize: 16,
 		marginTop: 3,
-		fontWeight: 'bold',
+        fontWeight: 'bold',
 	},
 	caption: {
 		fontSize: 14,
-		lineHeight: 14,
-	},
+    },
+    card : {
+        marginTop: 10,
+        marginBottom: 10,
+        marginRight: 10,
+        borderRadius: 10
+    }
 })
