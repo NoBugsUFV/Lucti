e8216440926c50271b008c5d7f573d3b8190fda3
Júlio Alves <julio619alves@gmail.com>
2020-07-10 07:20:06 -0300

    updates drawer content to receives userToken

--- mobile/src/pages/Template/DrawerContent.js
+++ mobile/src/pages/Template/DrawerContent.js
@@ -1,62 +1,35 @@
 import React from 'react';
 import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
-import AsyncStorage from '@react-native-community/async-storage';
 import {Avatar, Title, Caption, Drawer, Text, TouchableRipple, Switch, DefaultTheme, DarkTheme, Provider as PaperProvider} from 'react-native-paper'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import firebaseService from '../../services/Firebase/firebaseService';
+import { AuthContext } from '../../../components/context';
+import { useNavigation } from '@react-navigation/native';
 
 export function DrawerContent(props) {
+
+	const navigator = useNavigation();
+
 	const [isDarkTheme, setIsDarkTheme] = React.useState(false);
 
 	const toggleTheme = () => {
 		setIsDarkTheme(!isDarkTheme);
 	}
 
-	const darkTheme = {
-		...DarkTheme,
-		roundness: 2,
-		colors: {
-		  ...DarkTheme.colors,
-		  primary: '#3B5C2F',
-		  accent: '#76AB82',
-		},
-	};
-
-	const defaultTheme = {
-		...DefaultTheme,
-		roundness: 2,
-		colors: {
-			...DefaultTheme.colors,
-			primary: '#3B5C2F',
-			accent: '#76AB82',
-		},
-	};
-
-	async function signOutUser(){
-        try {
-            firebaseService.signOut();
-            await AsyncStorage.removeItem("@userData");
-			props.navigation.navigate("Login");
-        } catch(error){
-            console.log(error);
-        }
-    }
-
 	const [user, setUser] = React.useState();
 
-	getData = async () => {
-		try {
-			const value = await AsyncStorage.getItem('@userData')
-			if(value !== null) {
-				return JSON.parse(value)
-			}
-		} catch(e) {
-		  console.log(e)
-		}
+	if(user == null && props.userToken.userToken.user !== null){
+		setUser(JSON.parse(props.userToken.userToken.user))
+		navigator.navigate("Home");
 	}
+	
+	const { signOut } = React.useContext(AuthContext) 
 
-	Promise.resolve(getData()).then(res => setUser(res)).catch(err => console.error(err));
+	const signOutHandle = () => {
+		signOut()
+		setUser(null)
+		navigator.navigate("Login")
+	}
 	
     return (
 		<PaperProvider theme={isDarkTheme ? darkTheme : defaultTheme}>
@@ -67,8 +40,8 @@ export function DrawerContent(props) {
 							<TouchableOpacity style={{flexDirection:'column', alignItems: 'center', marginTop:15}} onPress={() => {props.navigation.navigate(user ? 'Profile' : 'Login')}}>
 								<Avatar.Image source={{uri: 'https://avatars3.githubusercontent.com/u/62998385?s=200&v=4'}} size={user ? 100 : 0}/>
 								<View style={{flexDirection: 'column', alignItems: 'center'}}>
-									<Title style={styles.title}>{user ? user.email : 'Acesse sua conta agora!'}</Title>
-									<Caption style={styles.caption}>{user ? user.email : 'Clique aqui'}</Caption>
+									<Title style={styles.title}>{user ? user.user.email : 'Acesse sua conta agora!'}</Title>
+									<Caption style={styles.caption}>{user ? user.user.email : 'Clique aqui'}</Caption>
 								</View>
 							</TouchableOpacity>
 						</View>
@@ -152,13 +125,33 @@ export function DrawerContent(props) {
 						}
 						label={user ? "Desconectar" : "Conectar"}
 						labelStyle={{color: isDarkTheme ? 'white' : '#202020' }}
-						onPress={() => {user ? signOutUser() : props.navigation.navigate('Login')}}
+						onPress={() => {user ? signOutHandle() : props.navigation.navigate('Login')}}
 					/>
 				</Drawer.Section>
 			</View>
 		</PaperProvider>
     );
 }
+
+const darkTheme = {
+	...DarkTheme,
+	roundness: 2,
+	colors: {
+	  ...DarkTheme.colors,
+	  primary: '#3B5C2F',
+	  accent: '#76AB82',
+	},
+};
+
+const defaultTheme = {
+	...DefaultTheme,
+	roundness: 2,
+	colors: {
+		...DefaultTheme.colors,
+		primary: '#3B5C2F',
+		accent: '#76AB82',
+	},
+};
 
 const styles = StyleSheet.create({
 	drawerContent: {
